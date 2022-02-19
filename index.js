feb2cc118eb12a27b637a70dea787f3aee3291ad
Daniil Bastrich <dbastrich@samokat.ru>
2022-02-19 15:24:22 +0300

    Add dependencies

--- index.js
+++ index.js
@@ -9,15 +9,31 @@ const path = require("path");
 /**
  * App Variables
  */
+const app = express();
+const port = process.env.PORT || "8000";
 
 /**
  *  App Configuration
  */
 
+
+
 /**
  * Routes Definitions
  */
 
+app.get("/", (req, res) => {
+    res.status(200).send("WHATABYTE: Food For Devs");
+});
+
+app.get("/getData", (req, res) => {
+    res.status(200).send("Some Data");
+});
+
 /**
  * Server Activation
  */
+
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
